2538ca94a65af81361ff9377365a0744f2880aa4
Martin Mengo <tinchomengo@gmail.com>
2024-12-13 00:15:56 -0300

    fix: updated contact section

--- src/components/pageSeven/index.jsx
+++ src/components/pageSeven/index.jsx
@@ -10,8 +10,7 @@ function PageSeven() {
         <p className='subtittle-page'>Take the Next Step toward a Secure and Strategic Trading Partnership</p>
         <p className='paragraph-page'>Ready to achieve stability and growth with Swiss confidence? Partner with Futura Capital AG for secure, strategic trading solutions rooted in Swiss finance principles.</p>
         <div className='contact-container'>
-          <Core image={res_1} tittle="Email" description="email@email.com"/>
-          <Core image={res_2} tittle="Phone number" description="XXX.XXX.XXX"/>
+          <Core image={res_1} tittle="Email" description="aman@futuracapital.co"/>
         </div>
     </div>
   )
